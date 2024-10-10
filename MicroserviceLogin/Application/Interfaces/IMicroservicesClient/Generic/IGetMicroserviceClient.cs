71d1c2d1d0d8e8ba1eb5861b321e671c866d4545
nicogarcette <nicolasgarcette@outlook.com>
2024-10-10 19:02:45 -0300

    remove using inncesaries

--- MicroserviceLogin/Application/Interfaces/IMicroservicesClient/Generic/IGetMicroserviceClient.cs
+++ MicroserviceLogin/Application/Interfaces/IMicroservicesClient/Generic/IGetMicroserviceClient.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.Interfaces.IMicroservices.Generic
+﻿namespace Application.Interfaces.IMicroservices.Generic
 {
     public interface IGetMicroserviceClient
     {
